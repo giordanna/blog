04063bc8b8180337eb20a6a515b6c69a2cfb221e
elangovanshanthi <elangovan2me@gmail.com>
2019-06-22 13:04:39 +0530

    initial code blog

---
+++ src/pages/blog/ajay.md
@@ -0,0 +1,7 @@
+---
+path: /blog/ajay
+title: Ajay
+date: 2018-12-29
+---
+
+This is  ajay blog.
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Button from "../components/button"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <Button buttonText="Click me" />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
